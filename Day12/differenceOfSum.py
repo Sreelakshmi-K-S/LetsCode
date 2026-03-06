d20909387e94065aec841bc1b0df539929bb4287
Sreelakshmi-K-S <sreelakshmiks2325@gmail.com>
2026-03-06 21:05:19 +0530

    differenceOfSum.py
    Implement a solution to calculate the difference between the sum of elements and the sum of their digits.

---
+++ Day12/differenceOfSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def differenceOfSum(self, nums: List[int]) -> int:
+        esum=0
+        dsum=0
+        for i in nums :
+            esum+=i
+            while i>0 :
+                dig=i%10
+                dsum+=dig
+                i//=10
+        return abs(esum-dsum) 
+        
